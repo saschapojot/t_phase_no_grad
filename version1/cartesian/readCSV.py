6704397e03e5c8d13a64709be26b51e9af2a922e
Xi LIU <xi.liu@u.nus.edu>
2024-06-09 17:01:59 +0800

    read csv

---
+++ version1/cartesian/readCSV.py
@@ -0,0 +1,37 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from pathlib import Path
+import sys
+from scipy.optimize import root
+
+#python readCSV.py path rowNum
+if len(sys.argv)!=3:
+    print("wrong number of arguments")
+
+fileName=sys.argv[1]
+rowNum=int(sys.argv[2])
+
+dfStr=pd.read_csv(fileName)
+
+oneRow=dfStr.iloc[rowNum,:]
+
+a1=float(oneRow.loc["a1"])
+a2=float(oneRow.loc["a2"])
+c1=float(oneRow.loc["c1"])
+c2=float(oneRow.loc["c2"])
+mA=float(oneRow.loc["mA"])
+mB=float(oneRow.loc["mB"])
+
+
+
+
+
+
+
+print("a1"+str(a1)+"a2"+str(a2)+"c1"+str(c1)+"c2"+str(c2)+"mA"+str(mA)+"mB"+str(mB))
+
+
+
+
+
